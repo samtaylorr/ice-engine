bc2518d50d5458849e4a27a527cb6c29f4472473
samtaylorr <taylorsam2401@gmail.com>
2024-10-25 06:32:31 +0100

    made scene a pointer

--- lib/main.cpp
+++ lib/main.cpp
@@ -19,17 +19,17 @@ bool loadMedia();
 void close();
 
 std::shared_ptr<LWindow> window = std::make_unique<LWindow>(Settings::SCREEN_WIDTH, Settings::SCREEN_HEIGHT);
-Scene currentScene;
+std::unique_ptr<Scene> currentScene = std::make_unique<Scene>();
 
 //Globally used font
 // Can't use RAII because of SDL being C-compatible (ew)
 TTF_Font* gFont = TTF_OpenFont( "fonts/easvhs.ttf", 14 );
 //Rendered texture
-LFont testText = LFont(window, currentScene, "predev", gFont, {0,0,0,0});
+LFont testText = LFont(window, *currentScene, "predev", gFont, {0,0,0,0});
 
 //                                                                                                       Spritesheet                      Spritesheet instructions
-std::shared_ptr<AnimatedSprite2D> animSprite = std::make_unique<AnimatedSprite2D>(window, currentScene, "img/main_character_sprite.png", "img/main_character_sprite.json");
-std::unique_ptr<PlayerController> player     = std::make_unique<PlayerController>(currentScene, animSprite);
+std::shared_ptr<AnimatedSprite2D> animSprite = std::make_unique<AnimatedSprite2D>(window, *currentScene, "img/main_character_sprite.png", "img/main_character_sprite.json");
+std::unique_ptr<PlayerController> player     = std::make_unique<PlayerController>(*currentScene, animSprite);
 
 bool loadMedia()
 {
@@ -47,7 +47,7 @@ void close()
 
 int main( int argc, char* args[] )
 {
-    currentScene.CreateMessage(SCENE_START);
+    currentScene->CreateMessage(SCENE_START);
     testText.SetTransform({5,0});
 
     while( !Input::HasInitiatedQuit() )
@@ -58,7 +58,7 @@ int main( int argc, char* args[] )
         Input::ProcessInput();
 
         //Update currentScene
-        currentScene.CreateMessage(SCENE_UPDATE);
+        currentScene->CreateMessage(SCENE_UPDATE);
 
         //Update screen
         window->Render();
